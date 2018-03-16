fea9626833472ca8d4f46338ba9e7ea369fb6d9b
Nathaniel Zeeman <nathanielzeeman@gmail.com>
2018-03-15 18:01:24 -0700

    Changed pickup prefab to only rotate the sprite instead of the whole object

--- Assets/Scripts/PickupBehavior.cs
+++ Assets/Scripts/PickupBehavior.cs
@@ -8,12 +8,14 @@ public class PickupBehavior : MonoBehaviour
     public float bobDistance;
 
     public uint Index { get; set; }
+    private GameObject sprite;
     private PickupsManager manager;
     private float originalY;
 
     // Use this for initialization
     void Start ()
     {
+        sprite = transform.GetChild(0).gameObject;
         originalY = transform.position.y;
         manager = GetComponentInParent<PickupsManager>();
 	}
@@ -21,7 +23,7 @@ public class PickupBehavior : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        transform.Rotate(new Vector3(0, 0, Time.deltaTime) * rotationSpeed);
+        sprite.transform.Rotate(new Vector3(0, 0, Time.deltaTime) * rotationSpeed);
 
         transform.position = new Vector2(transform.position.x,
             originalY + Mathf.Sin(Time.time) * bobDistance);
